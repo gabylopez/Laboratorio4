e1247e87842f3d7682d38f32740d1c0b81ef7ed9
Gaby <Gaby@Gaby-PC.lan>
2014-08-08 02:56:51 -0600

    New Commit

---
+++ src/lab4/StackAbs.java
@@ -0,0 +1,16 @@
+//------------------------------------
+//StackAbs
+// Augusto Cárdenas 13136
+// Maria Gabriela Lopez 13056
+// Andres Lainfiesta 13072
+// 9 de Agosto de 2014
+//------------------------------------
+
+abstract public class StackAbs<E> implements Stack<E> {
+     public boolean vacio()
+   // Stack vacio entonces True
+   {
+      return size() == 0;
+   }
+}
+	
